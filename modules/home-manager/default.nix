2fcb004df06aa6eb92738bbf4c24a449e2e3a51f
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-12-27 18:15:19 +0100

    forgot to add module and ordered some things

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -5,39 +5,42 @@
     ./packages
 
     # Toggleable (see below)
-    ./hyprland
-    ./waybar
-    ./extraDotfiles
-    ./zsh
-    ./tmux
-    ./dunst
     ./alacritty
-    ./htop
-    ./scripts
-    ./nvim
+    ./dunst
+    ./extraDotfiles
     ./gtk
+    ./htop
+    ./hyprland
+    ./misc
+    ./nvim
+    ./scripts
+    ./tmux
+    ./waybar
+    ./zsh
   ];
 
-  hyprlandModule.enable =
-    lib.mkDefault true;
-  waybarModule.enable =
-    lib.mkDefault true;
-  extraDotfilesModule.enable =
-    lib.mkDefault true;
-  zshModule.enable =
-    lib.mkDefault true;
-  tmuxModule.enable =
+  alacrittyModule.enable =
     lib.mkDefault true;
   dunstModule.enable =
     lib.mkDefault true;
-  alacrittyModule.enable =
+  extraDotfilesModule.enable =
+    lib.mkDefault true;
+  gtkModule.enable =
     lib.mkDefault true;
   htopModule.enable =
     lib.mkDefault true;
-  scriptsModule.enable =
+  hyprlandModule.enable =
+    lib.mkDefault true;
+  miscModule.enable =
     lib.mkDefault true;
   nvimModule.enable =
     lib.mkDefault true;
-  gtkModule.enable =
+  scriptsModule.enable =
+    lib.mkDefault true;
+  tmuxModule.enable =
+    lib.mkDefault true;
+  waybarModule.enable =
+    lib.mkDefault true;
+  zshModule.enable =
     lib.mkDefault true;
 }
