0f6fec3478a961a675987f305ac2ad7eb8604866
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-12-29 18:58:43 +0100

    modularizing my nixos config

--- hosts/nixlap/configuration.nix
+++ hosts/nixlap/configuration.nix
@@ -7,23 +7,8 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nixos/default.nix
   ];
-
-  nix = {
-    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-  };
-  nixpkgs.config.allowUnfree = true;
 
   boot = {
     loader.systemd-boot.enable = true;
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,11 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  imports = [];
+{lib, ...}: {
+  imports = [
+    # Always enabled
+    ./nix
+
+    # Toggleable (see below)
+  ];
+
+  nixModule.enable =
+    lib.mkDefault true;
 }
