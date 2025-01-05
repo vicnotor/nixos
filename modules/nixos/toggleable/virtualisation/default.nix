7402cda570844e23fcf5b4558d38e279d508ffa0
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2025-01-05 22:59:53 +0100

    trying out some virtualization

--- modules/nixos/toggleable/virtualisation/default.nix
+++ modules/nixos/toggleable/virtualisation/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options = {
@@ -9,10 +10,32 @@
   };
 
   config = lib.mkIf config.virtualisationModule.enable {
+    programs.dconf.enable = true;
+
     users.groups.libvirtd.members = ["vic"];
+    users.users.vic.extraGroups = ["libvirtd"];
+
+    environment.systemPackages = with pkgs; [
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      spice-protocol
+      win-virtio
+      win-spice
+    ];
+
     virtualisation = {
-      libvirtd.enable = true;
+      libvirtd = {
+        enable = true;
+        qemu = {
+          swtpm.enable = true;
+          ovmf.enable = true;
+          ovmf.packages = [pkgs.OVMFFull.fd];
+        };
+      };
       spiceUSBRedirection.enable = true;
     };
+    services.spice-vdagentd.enable = true;
   };
 }
