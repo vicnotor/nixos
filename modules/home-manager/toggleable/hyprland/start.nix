92d3757e8b72988bde3fa2528116814089f24456
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2025-01-07 12:01:23 +0100

    comments

--- modules/home-manager/toggleable/hyprland/start.nix
+++ modules/home-manager/toggleable/hyprland/start.nix
@@ -2,13 +2,17 @@
   # Fixes some apps with slow initial startup
   "[workspace special:firstlaunchapps silent] uwsm app -- $terminal"
   "[workspace special:firstlaunchapps silent] uwsm app -- $fileManager"
+
+  # Wallpaper
   "swww-daemon &"
   "wpdefault"
 
+  # Other startup apps
+  "uwsm app -- waybar &"
   "uwsm app -- nm-applet --indicator &"
   "uwsm app -- blueman-applet &"
   "uwsm app -- wl-gammarelay-rs &"
-  "uwsm app -- waybar &"
 
+  # Cursor
   "hyprctl setcursor BreezeX-RosePine-Linux 20"
 ]
