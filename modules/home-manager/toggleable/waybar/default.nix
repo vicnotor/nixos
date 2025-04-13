62f61928e50a7f0796f9269b99781da49091f849
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-04-13 23:09:14 +0200

    moved waybar out of home-manager

--- modules/home-manager/toggleable/waybar/default.nix
+++ modules/home-manager/toggleable/waybar/default.nix
@@ -1,7 +1,7 @@
 {
-  pkgs,
   lib,
   config,
+  pkgs,
   inputs,
   ...
 }: {
@@ -12,160 +12,18 @@
 
   config = lib.mkIf config.waybarModule.enable {
     home = {
+      packages = [
+        (
+          inputs.waybar.packages.${pkgs.stdenv.hostPlatform.system}.waybar.overrideAttrs
+          (oldAttrs: {
+            mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+          })
+        )
+      ];
       file = {
         ".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Git/vicnotor/nixos/modules/home-manager/toggleable/waybar/style.css";
+        ".config/waybar/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Git/vicnotor/nixos/modules/home-manager/toggleable/waybar/config";
       };
-    };
-
-    programs.waybar = {
-      enable = true;
-      package = (
-        inputs.waybar.packages.${pkgs.stdenv.hostPlatform.system}.waybar.overrideAttrs
-        (oldAttrs: {
-          mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-        })
-      );
-      settings = [
-        {
-          layer = "top";
-          margin = "0px";
-          height = 27;
-          modules-left = ["hyprland/workspaces"];
-          modules-center = ["hyprland/window"];
-          modules-right = [
-            "tray"
-            "pulseaudio#input"
-            "custom/seperator"
-            "pulseaudio#output"
-            "custom/seperator"
-            "backlight"
-            "custom/seperator"
-            "custom/wl-gammarelay"
-            "custom/seperator"
-            "memory"
-            "custom/seperator"
-            "temperature"
-            "custom/seperator"
-            "battery"
-            "custom/seperator"
-            "clock"
-            "custom/rofi"
-          ];
-          "hyprland/workspaces" = {
-            format = "{icon}";
-            on-click = "activate";
-            sort-by-number = true;
-          };
-          "hyprland/window" = {
-            format = "";
-            max-length = 50;
-            separate-outputs = true;
-            hide-empty-text = true;
-          };
-          "custom/wl-gammarelay" = {
-            format = "  ";
-            exec = "wl-gammarelay-rs watch {t}";
-            on-scroll-up = "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n +100";
-            on-scroll-down = "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n -100";
-            on-click-right = "busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500";
-            on-click-middle = "busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500";
-            tooltip = true;
-            tooltip-format = " {}K";
-          };
-          "custom/seperator" = {
-            format = "|";
-            interval = "once";
-            tooltip = false;
-          };
-          "custom/rofi" = {
-            format = "  ";
-            on-click = "rofi -show drun -show-icons -run-command 'uwsm app -- {cmd}'";
-            tooltip = false;
-          };
-          tray = {
-            icon-size = 16;
-            spacing = 6;
-            reverse-direction = true;
-          };
-          backlight = {
-            format = "{icon} {percent}%";
-            format-icons = ["󰃞" "󰃟" "󰃠"];
-            on-click-right = "brightnessctl s 50%";
-            on-click-middle = "brightnessctl s 50%";
-          };
-          clock = {
-            interval = 1;
-            format = "{:%d-%m %H:%M:%S}";
-            tooltip-format = "<tt><small>{calendar}</small></tt>";
-            calendar = {
-              "mode" = "month";
-              "mode-mon-col" = 3;
-              "weeks-pos" = "right";
-              "on-scroll" = 1;
-              "format" = {
-                "months" = "<span color='#ffead3'><b>{}</b></span>";
-                "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-                "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-                "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-                "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
-              };
-            };
-          };
-          cpu = {
-            format = "C {usage}%";
-            tooltip = false;
-          };
-          memory = {
-            format = "󰍛 {}%";
-            on-click = "taskmanager";
-          };
-          temperature = {
-            thermal-zone = 5;
-            critical-threshold = 80;
-            format = "{icon} {temperatureC}°C";
-            format-icons = ["" "" ""];
-            on-click = "taskmanager";
-          };
-          battery = {
-            interval = 10;
-            states = {
-              warning = 25;
-              critical = 15;
-            };
-            format = "{icon}  {capacity}%";
-            format-full = " {capacity}%";
-            format-charging = "󱐌 {capacity}%";
-            format-plugged = " {capacity}%";
-            format-icons = ["" "" "" "" ""];
-          };
-          "pulseaudio#output" = {
-            format = "{icon} {volume}%";
-            format-muted = "";
-            format-icons = {
-              headphone = "";
-              headset = "";
-              phone = "";
-              portable = "";
-              car = "";
-              default = ["" "" ""];
-            };
-            on-click = "pavucontrol";
-            on-click-right = "wpctl set-mute @DEFAULT_SINK@ toggle";
-            on-click-middle = "wpctl set-mute @DEFAULT_SINK@ toggle";
-          };
-          "pulseaudio#input" = {
-            format-source = "󰍬 {volume}%";
-            format-source-muted = "󰍭";
-            format = "{format_source}";
-            on-click = "pavucontrol";
-            on-click-right = "wpctl set-mute @DEFAULT_SOURCE@ toggle";
-            on-click-middle = "wpctl set-mute @DEFAULT_SOURCE@ toggle";
-            on-scroll-up = "wpctl set-volume @DEFAULT_SOURCE@ 1%+";
-            on-scroll-down = "wpctl set-volume @DEFAULT_SOURCE@ 1%-";
-            tooltip-format = "{desc}";
-          };
-        }
-      ];
     };
   };
 }
