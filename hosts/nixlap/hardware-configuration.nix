cda698875915fe087698bd6094e79594708550a2
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-04-14 21:05:40 +0200

    installed nixos on other ssd, so changed hardware-configuration.nix

--- hosts/nixlap/hardware-configuration.nix
+++ hosts/nixlap/hardware-configuration.nix
@@ -8,24 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "vmd" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "vmd" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c5dbf4ed-b479-4c8d-b96b-584eb8490933";
+    { device = "/dev/disk/by-uuid/b40dbc1a-94e7-4d4d-a022-f15c6fc29f75";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B12B-C17B";
+    { device = "/dev/disk/by-uuid/8F8A-ADBF";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/fe6dc634-d2a7-4ae8-a53c-2074d3971fb8"; }
+    [ { device = "/dev/disk/by-uuid/0be4f20e-f068-4618-9757-ce975ba75a24"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
--- modules/home-manager/always/packages/default.nix
+++ modules/home-manager/always/packages/default.nix
@@ -4,7 +4,6 @@
   ...
 }: {
   home.packages = with pkgs; [
-    aseprite
     brightnessctl
     comma # Wrapper for nix shell -c
     discord
