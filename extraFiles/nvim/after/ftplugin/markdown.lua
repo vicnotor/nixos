2ca5e603ee4ab196bf3a6d101f1b942fc72c89ca
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-04-22 10:22:15 +0200

    fixed markdown shiftwidth

--- extraFiles/nvim/after/ftplugin/markdown.lua
+++ extraFiles/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,1 @@
--- vim.keymap.set("n", "<leader>f", function()
--- 	require("conform").format({ lsp_fallback = true, quiet = true })
--- 	local _, _ = pcall(function()
--- 		vim.cmd([[:silent! %s/\\\[\(.*\)\\\]/[\1]/g]])
--- 	end)
--- end, { desc = "Format file" })
+vim.opt.shiftwidth = 2
