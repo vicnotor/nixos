00a1e93e3fdfa47871492e3a9425616c8e853b8e
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-08 22:32:02 +0200

    gaming

--- modules/nixos/always/misc/default.nix
+++ modules/nixos/always/misc/default.nix
@@ -24,10 +24,10 @@
   };
 
   # Mounting SATA drive
-  boot.supportedFilesystems = ["ntfs"];
-  fileSystems."/home/vic/mnt/sata" = {
-    device = "/dev/disk/by-label/SATA";
-    fsType = "ntfs-3g";
-    options = ["rw" "uid=1000"];
-  };
+  # boot.supportedFilesystems = ["ntfs"];
+  # fileSystems."/home/vic/mnt/sata" = {
+  #   device = "/dev/disk/by-label/SATA";
+  #   fsType = "ntfs-3g";
+  #   options = ["rw" "uid=1000"];
+  # };
 }
