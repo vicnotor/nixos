1c199253bfd3049a47707fc951f218fd9a661855
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-10 16:17:20 +0200

    update

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
     nixd.url = "github:nix-community/nixd";
     ghostty.url = "github:ghostty-org/ghostty?rev=7f9bb3c0e54f585e11259bc0c9064813d061929c";
     # ghostty.url = "github:weskoerber/ghostty";
-    zig.url = "github:mitchellh/zig-overlay";
     stylix.url = "github:danth/stylix";
     nix-devshells = {
       url = "github:vicnotor/nix-devshells";
