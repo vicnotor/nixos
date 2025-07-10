049b7ab719765fa618b7e9b301f3e20c8b751489
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-10 14:29:10 +0200

    dev env commit

--- modules/nixos/always/environment/default.nix
+++ modules/nixos/always/environment/default.nix
@@ -3,7 +3,6 @@
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
       NH_FLAKE = "/home/vic/Git/vicnotor/nixos";
-      TERMINAL = "ghostty";
     };
     localBinInPath = true;
     etc."nsswitch.conf".text = ''
