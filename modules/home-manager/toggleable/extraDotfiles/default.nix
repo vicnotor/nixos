615ad71a088b51e87bc3371bf00f89d6182c784e
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-07-29 18:25:32 +0200

    automated dev commit

--- modules/home-manager/toggleable/extraDotfiles/default.nix
+++ modules/home-manager/toggleable/extraDotfiles/default.nix
@@ -15,6 +15,7 @@
         export XCURSOR_SIZE,20
         export AQ_DRM_DEVICES="/dev/dri/card1:/dev/dri/card0"
         export GDK_SCALE=1 # Change this to 2 if content of xwayland windows is too small
+        export APP2UNIT_SLICES='a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice'
       '';
       ".config/onedrive/config".text = ''
         sync_dir="~/personal"
