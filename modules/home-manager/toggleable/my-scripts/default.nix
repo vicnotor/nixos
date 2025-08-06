7bd41fa8dd801375beabf689182900bef9c72128
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-08-06 17:13:54 +0200

    automated dev commit

--- modules/home-manager/toggleable/my-scripts/default.nix
+++ modules/home-manager/toggleable/my-scripts/default.nix
@@ -4,11 +4,11 @@
   ...
 }: {
   options = {
-    my-sciptsModule.enable =
+    my-scriptsModule.enable =
       lib.mkEnableOption "My shell scripts module";
   };
 
-  config = lib.mkIf config.my-sciptsModule.enable {
+  config = lib.mkIf config.my-scriptsModule.enable {
     home = {
       file = {
         ".local/bin".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Git/vicnotor/nixos/modules/home-manager/toggleable/my-scripts";
