cfcc1a2ec38f0bb1bd133a848c827c93ef199993
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-08-15 23:05:16 +0200

    automated dev commit

--- hosts/nixvirt/home.nix
+++ hosts/nixvirt/home.nix
@@ -1,7 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   imports = [../../modules/home-manager];
 
   home.stateVersion = "25.05"; # NOTE: CHANGE BASED ON NIXOS INSTALL ISO VERSION
 
+  latexModule.enable = false;
+  libreofficeModule.enable = false;
   minecraftModule.enable = false;
+  mpvModule.enable = false;
+
+  # Browser
+  zenModule.enable = false;
+  home.packages = [pkgs.firefox];
 }
