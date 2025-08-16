770150b30925202346e83f9d2b58f1be979ee825
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-08-16 14:21:27 +0200

    automated dev commit

--- modules/home-manager/toggleable/nvim/lua/plugins/treesitter.lua
+++ modules/home-manager/toggleable/nvim/lua/plugins/treesitter.lua
@@ -8,49 +8,18 @@ return {
         opts = { max_lines = 3 }
       },
     },
-    build = ":TSUpdate",
+    branch = "master",
     lazy = false,
+    build = ":TSUpdate",
     config = function()
-      -- See `:help nvim-treesitter`
-      -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-      vim.defer_fn(function()
-        require("nvim-treesitter.configs").setup({
-          -- Add languages to be installed here that you want installed for treesitter
-          ensure_installed = {
-            "c",
-            "cpp",
-            "go",
-            "lua",
-            "python",
-            "rust",
-            "javascript",
-            "typescript",
-            "vimdoc",
-            "vim",
-            "bash",
-            "r",
-            "markdown",
-            "rnoweb",
-            "yaml",
-            "latex",
-            "regex",
-          },
-
-          -- Install parsers synchronously (only applied to `ensure_installed`)
-          sync_install = false,
-
-          -- Autoinstall languages that are not installed. Defaults to false.
-          auto_install = true,
-
-          highlight = {
-            enable = true,
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            additional_vim_regex_highlighting = false,
-          },
-          indent = { enable = true, disable = { "ocaml" } },
-        })
-      end, 0)
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true, disable = { "ocaml" } },
+      })
     end,
   },
 }
