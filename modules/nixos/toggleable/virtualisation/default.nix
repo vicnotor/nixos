cc1afb69222670430d41bd41c30148a26739a3ee
vicnotor <95343562+vicnotor@users.noreply.github.com>
2025-11-25 09:58:22 +0100

    automated dev commit

--- modules/nixos/toggleable/virtualisation/default.nix
+++ modules/nixos/toggleable/virtualisation/default.nix
@@ -27,7 +27,6 @@
       docker.enable = true;
       podman = {
         enable = true;
-        dockerCompat = true;
         defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
       };
     };
